4b656d21daf9dbf6b8f99fab2a3aca3e25a49c05
Samantha Diaz <samidiaz3@gmail.com>
2022-03-27 22:06:36 -0400

    deleted some testing folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Switch,  Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import TripsContainer from './containers/TripsContainer';
-import Home from './components/Home'
 import Container from 'react-bootstrap/Container';
 
 
@@ -18,28 +16,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// export default App;
-
