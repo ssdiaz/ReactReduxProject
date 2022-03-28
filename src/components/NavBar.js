04371abe556030b368ac83f063b46badcd4ec5c2
Samantha Diaz <samidiaz3@gmail.com>
2022-03-27 20:27:20 -0400

    removed container div in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,10 @@ import {  NavLink } from "react-router-dom";
 const NavBar = () => {
 
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink to='/' className="nav-link" >HOME</NavLink>
-        <NavLink to='/trips' className="nav-link" >TRIPS</NavLink>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink to='/' className="nav-link" >HOME</NavLink>
+      <NavLink to='/trips' className="nav-link" >TRIPS</NavLink>
+    </nav>
   )
 }
 
